08e253e726d46c855acf3d206d64357af95209df
cetteup <cetteup@dasemail.de>
2020-07-24 22:55:16 +0200

    Added manual nslookup to spread gslist retried across servers

--- generate-json-serverlist.py
+++ generate-json-serverlist.py
@@ -1,22 +1,28 @@
 import json
-import re
 import subprocess
 import sys
 import logging
 
+from nslookup import Nslookup
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+# Manually look up servers.bf2hub.com to be able to spread retried across servers
+lookerUpper = Nslookup()
+dnsResult = lookerUpper.dns_lookup('servers.bf2hub.com')
 
 # Run gslist and capture output
 commandOk = False
 tries = 0
 maxTries = 3
-result = None
+gslistResult = None
 while not commandOk and tries < maxTries:
+    # Alternate between first and last found A record
+    serverIp = dnsResult.answer[0] if tries % 2 == 0 else dnsResult.answer[-1]
     try:
-        logging.info('Running gslist command')
-        result = subprocess.run(['gslist', '-n', 'battlefield2', '-x', 'servers.bf2hub.com:28911',
-                                 '-Y', 'battlefield2', 'hW6m9a', '-o', '1'],
-                                capture_output=True, timeout=10)
+        logging.info(f'Running gslist command against {serverIp}')
+        gslistResult = subprocess.run(['gslist', '-n', 'battlefield2', '-x', f'{serverIp}:28911',
+                                       '-Y', 'battlefield2', 'hW6m9a', '-o', '1'], capture_output=True, timeout=10)
         commandOk = True
     except subprocess.TimeoutExpired as e:
         logging.error(f'gslist timed out, try {tries + 1}/{maxTries}')
@@ -24,7 +30,7 @@ while not commandOk and tries < maxTries:
 
 # Make sure any server were found
 # (gslist sends all output to stderr so check there)
-if result is None or 'servers found' not in str(result.stderr):
+if gslistResult is None or 'servers found' not in str(gslistResult.stderr):
     sys.exit('gslist could not retrieve any servers')
 
 # Read gslist output file
