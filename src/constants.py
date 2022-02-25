6eb959872ac91c6c6a5cd0a273b303d304c15ca8
cetteup <me@cetteup.com>
2022-02-25 19:01:44 +0100

    feat: Add OpenSpy principal to BF2

--- src/constants.py
+++ src/constants.py
@@ -113,7 +113,7 @@ GAMESPY_CONFIGS = {
         'encType': '-1',
         'superQueryType': '8',
         'port': 28910,
-        'servers': ['bf2hub', 'phoenixnetwork', 'playbf2'],
+        'servers': ['bf2hub', 'openspy', 'phoenixnetwork', 'playbf2'],
         'gamedigType': 'bf2'
     },
     'postal2': {
