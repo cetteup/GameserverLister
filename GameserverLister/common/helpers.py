be9994967c1e0d24a80dc2c6998d4400e965ddba
cetteup <me@cetteup.com>
2025-12-26 11:15:23 +0100

    feat: accept SWAT4 First Responders Tactical Edition mod when verifying servers

--- GameserverLister/common/helpers.py
+++ GameserverLister/common/helpers.py
@@ -115,7 +115,7 @@ def is_server_for_gamespy_game(game: GamespyGame, game_name: str, parsed_result:
         return parsed_result.get('gamename') == game_name and parsed_result.get('gamevariant') == 'fh2'
     elif game is GamespyGame.SWAT4:
         # SWAT 4 has a very limited set of keys, so we need to look at values
-        return parsed_result.get('gamevariant') in ['SWAT 4', 'SWAT 4X', 'SEF', 'FR']
+        return parsed_result.get('gamevariant') in ['SWAT 4', 'SWAT 4X', 'SEF', 'FR', 'FRTE']
     elif game is GamespyGame.UT3:
         # UT3 has some *very* unique keys, see https://github.com/gamedig/node-gamedig/blob/master/protocols/ut3.js#L7
         return 'p1073741825' in parsed_result and 'p1073741826' in parsed_result
